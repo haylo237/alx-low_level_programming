1dd448e44e270f23da63febcaf2920d7226b9d7c
haylo237 <njikeabednego@hotmail.com>
2023-07-14 13:46:19 -0700

    Task 4 commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,16 +6,17 @@
  */
 int main(void)
 {
-	char ch;
+	char ch = 'a';
 
-	for (ch = 'a' ; ch <= 'z' ; ch++)
+	while (ch <= 'z')
 	{
 		if (ch == 'e' || ch == 'q')
 		{
-			ch = ch + 1;
+			ch++;
 			continue;
 		}
 	putchar(ch);
+	ch++;
 	}
 	putchar('\n');
 	return (0);

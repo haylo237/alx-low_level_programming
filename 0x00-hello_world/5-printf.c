fb7307073696237185a036b956adbd070092eac6
haylo237 <njikeabednego@hotmail.com>
2023-07-13 18:12:30 -0700

    commit for task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - This is where the program starts
+ *
+ * Return: O means Success
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
+

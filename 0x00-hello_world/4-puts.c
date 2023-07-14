09f2f26ae58a52d98536699cc1ceee57e5d0c57f
haylo237 <njikeabednego@hotmail.com>
2023-07-13 17:31:14 -0700

    commit for task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,8 @@
  *
  * return: always 0
  */
-int main(0)
+int main(void)
 {
-/*This code prints a text using puts*/
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }

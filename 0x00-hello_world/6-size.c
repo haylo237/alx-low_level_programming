09aa1ff4feb0435072cc15e7e65a932bd34283da
haylo237 <njikeabednego@hotmail.com>
2023-07-13 18:49:55 -0700

    task 6 commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry of the program
+ * Return: The value 0 mean success
+ */
+int main(void)
+{
+printf("Size of char: %c byte(s)", sizeof(char));
+printf("\nSize of int: %d byte(s)", sizeof(int));
+printf("\nSize of long int: %li byte(s)", sizeof(long int);
+printf("\nSize of long long int: %lli byte(s)", sizeof(long long int));
+printf("\nSize of float: %f byte(s)", sizeof(float));
+return (0);
+}

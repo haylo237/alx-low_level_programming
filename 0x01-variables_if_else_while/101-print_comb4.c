3d7945fd50f30c2660fb36524465b1a76ee8c815
haylo237 <njikeabednego@hotmail.com>
2023-07-14 19:35:58 -0700

    task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - program start
+ * Return: 0 is Success
+ */
+
+int main(void)
+{
+	int f, m, l;
+
+	for (f = 48 ; f < 57 ; f++)
+	{
+		for (m = f + 1 ; m <= 57 ; m++)
+		{
+			for (l = m + 1 ; l <= 57 ; l++)
+			{
+				if ((f != m) != l)
+				{
+					putchar(f);
+					putchar(m);
+					putchar(l);
+					if (f == 55 && m == 56)
+						continue;
+						putchar(44);
+						putchar(32);
+				}
+			}
+		}
+	}
+	putchar('\n')
+	return (0);
+}

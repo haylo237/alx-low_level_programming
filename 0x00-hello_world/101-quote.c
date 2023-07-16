948ac05c05cedb109db8a23aae56edd48ca7b9e9
haylo237 <njikeabednego@hotmail.com>
2023-07-15 19:26:34 -0700

    commit task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Start of the program
+ * Description: program that prints
+ * Return: Success is 0
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}

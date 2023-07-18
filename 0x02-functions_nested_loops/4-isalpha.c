fc882a642a0c9a5844b49eb272ad51098428af07
haylo237 <njikeabednego@hotmail.com>
2023-07-18 14:17:56 -0700

    Commit task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,12 +6,9 @@
  */
 int _isalpha(int c)
 {
-	if (c >= 65 && c <= 90)
+	if ((c >= 65 && c <= 90) || (c >= && c <= 98))
 	{
-		if (c >= 97 && c <= 122)
-		{
-			return (1);
-		}
+		return (1);
 	}
 	else
 	{

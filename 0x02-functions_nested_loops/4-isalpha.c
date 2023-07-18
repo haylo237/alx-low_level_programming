dd8350e06f84663eac13e067ac1b381a08f70f57
haylo237 <njikeabednego@hotmail.com>
2023-07-18 13:53:36 -0700

    commit for task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * _isalpha - Determines if c is a letter
+ * @c: ASCII character
+ * Return: 1 if c is lower or upper. 0 for others
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		if (c >= 97 && c <= 122)
+		{
+			return (1);
+		}
+	}
+	else
+	{
+		return (0);
+	}
+}

16f46646355b4b0fe3b7169f9d385ed43feab158
haylo237 <njikeabednego@hotmail.com>
2023-07-18 08:35:04 -0700

    first commit for this progject

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - entry to te program
+* Description: print putchar
+* Return: 0 is success
+*/
+int main (void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This README is for function and nested loops

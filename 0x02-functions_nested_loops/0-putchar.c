056fa0a4788ee261a20854bdcb095256ec0bc891
haylo237 <njikeabednego@hotmail.com>
2023-07-18 09:25:59 -0700

    task 0 commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
-#include <main.h>
+#include "main.h"
 /**
 * main - entry to te program
 * Description: print putchar
 * Return: 0 is success
 */
-int _putchar (void)
+int main (void)
 {
-	printf("_putchar\n");
+	_putchar("95");
+	_putchar("112");
+	_putchar("117");
+	_putchar("116");
+	_putchar("99");
+	_putchar("104");
+	_putchar("97");
+	_putchar("114");
+	_putchar('\n');
 	return (0);
 }

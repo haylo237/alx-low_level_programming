2c0cfb6266484d8e4e824dc12ec2fd152f032418
haylo237 <njikeabednego@hotmail.com>
2023-07-18 16:33:13 -0700

    commit for task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - The function that runs
+ * @i: the variable to be checked
+ * Return: Return are absolute values
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		return (i * -1);
+	}
+	else
+	{
+		return (i);
+	}
+}

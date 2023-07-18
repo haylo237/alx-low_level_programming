d462c0376f1d51c6b2dc9569c9fcc22101aab755
haylo237 <njikeabednego@hotmail.com>
2023-07-18 11:37:41 -0700

    task 2 commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,7 @@ void print_alphabet_x10(void)
 {
 	int i = 0;
 	int c;
-	
+
 	while (i < 10)
 	{
 		for (c = 'a' ; c <= 'z' ; c++)
@@ -16,6 +16,5 @@ void print_alphabet_x10(void)
 		}
 		_putchar('\n');
 		i++;
-	}
-	
+	}	
 }

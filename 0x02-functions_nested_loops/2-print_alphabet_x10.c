7a4e9fbad48def5062c2aeb414a45641a0f0baee
haylo237 <njikeabednego@hotmail.com>
2023-07-18 11:18:54 -0700

    task 2 commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints aphabet 10x
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 1 ; i <= 10 ; i++)
+	{
+		print_alphabet(void);
+	}
+}

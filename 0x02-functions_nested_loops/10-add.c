607de21ac10a3de6393b421c707778fbb6a6f52d
haylo237 <njikeabednego@hotmail.com>
2023-07-19 15:24:39 -0700

    Task 10 commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - Adds 2 numbers
+ * @i: variable of value to be added
+ * @j: varaible of value to be added
+ * Return: Returns sum
+ */
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+	return (sum);
+}

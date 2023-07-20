9c9ef04704648444415dd0de19a9b51cd636fc69
haylo237 <njikeabednego@hotmail.com>
2023-07-19 19:43:22 -0700

    task 6 commit

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Return: Success is 0
+ */
+int main(void)
+{
+	long int f = 1;
+	long int s = 2;
+	long int n;
+	int i;
+
+	for (i = 2 ; i < 98 ; i++)
+	{
+		n = f + s;
+		printf("%li");
+		if (i != 97)
+			printf(", ");
+		f = s;
+		s = n;
+	}
+	printf("\n");
+}

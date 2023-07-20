f2c76aae16acfab0543d174845aa1c5bdabb739d
haylo237 <njikeabednego@hotmail.com>
2023-07-19 19:27:02 -0700

    task Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,38 +1,27 @@
 #include <stdio.h>
 /**
  * main - start of the execution
- * fibo_sum - Fibonacci sum
- * @l: limit of terms
  * Return: 0 is Success
  */
-long int fibo_sum(long int l);
-
 int main(void)
 {
-	int l = 4000000;
-	long int total_sum = fibo_sum(l);
-
-	printf("%li\n", total_sum);
-	return (0);
-}
-
-long int fibo_sum(long int l)
-{
 	long int f = 1;
-	long int s, n, sum;
+	long int s = 2;
+	long int sum = 0;
+	long int n;
+	int l = 4000000;
 	int i;
 
-	s = f + 1;
-
-	for (i = 0 ; n <= l ; i++)
+	while (f <= l)
 	{
-		n = s + f;
-		if (n % 2 == 0)
+		if (f % 2 == 0)
 		{
-			sum += n;
+			sum += f;
 		}
-		s = f;
+		n = f;
 		f = s;
+		s += n;
 	}
-	return (sum);
+	printf("%li\n", sum);
+	return (0);
 }

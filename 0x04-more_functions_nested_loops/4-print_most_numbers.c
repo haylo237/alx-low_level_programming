cdcd71d6c740ccb3308787c623423d0f2c5370ae
haylo237 <njikeabednego@hotmail.com>
2023-07-20 17:03:13 -0700

    task 4 commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints 9 - 0 except 2 & 4
+ * Return: 0 is Success
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0' ; i <= 9 ; i++)
+	{
+		if ((i != 2) || (i != 4))
+		_putchar(i);
+	}
+	_putchar('\n');
+}

5c320124ebcc152c9ce256d1268ee25ab55f67c3
haylo237 <njikeabednego@hotmail.com>
2023-07-21 19:53:06 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * print_diagonal - prints
+ * @n: The number of times to print lines
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int x;
+
+	for (x = 1 ; x <= 100 ; x++)
+	{
+		if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", x);
+		}
+		if (x != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}

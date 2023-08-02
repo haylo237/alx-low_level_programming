f2115a21dc699367308eb25c199a3f1f0b47f6f6
haylo237 <njikeabednego@hotmail.com>
2023-08-01 17:54:20 -0700

    Chess is mental torture

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_chessboard - entry point
+ * @a: array variable
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, n;
+
+	for (i = 0 ; i < 0 ; i++)
+	{
+		for (n = 0 ; n < 0 ; n++)
+		{
+			_putchar(s[i][n])
+		}
+		_putchar('\0');
+	}
+}

d9dad3c3de70f09c47337d48dcf0212daa8c007f
haylo237 <njikeabednego@hotmail.com>
2023-10-13 03:01:10 +0000

    task 6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * sum_dlistint - sum of data in list
+ * @head - head of the list
+ * Return: sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
+
+

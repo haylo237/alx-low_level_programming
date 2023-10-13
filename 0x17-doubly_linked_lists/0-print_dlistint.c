58d992dc670ab1dc8e6aad3887cdddbe497fb401
haylo237 <njikeabednego@hotmail.com>
2023-10-13 01:51:57 +0000

    First commit of this directory

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints the content of a 
+ * doubly linked list
+ * @h: Pointer to the head
+ * Return: Number of Nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t n_Nodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		n_Nodes++;
+	}
+
+	return (n_Nodes);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This directory is for the Doubly linked List implementations

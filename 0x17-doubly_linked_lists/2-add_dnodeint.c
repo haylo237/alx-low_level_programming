db23947b3b5cc97fd6d72c41cae796fb31615f2c
haylo237 <njikeabednego@hotmail.com>
2023-10-13 02:34:26 +0000

    task 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_n = malloc(sizeof(dlistint_t));
+
+	if (new_n == NULL)
+		return (NULL);
+
+	new_n->n = n;
+	new_n->prev = NULL;
+	new_n->next = *head;
+
+	if (*head != NULL)
+		(*head)->prev = new_n;
+
+	*head = new_n;
+
+	return (new_n);
+}
